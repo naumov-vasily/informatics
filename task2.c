b35ed37bc099530b3a44b9bd6d950cbd76530286
naumov-vasily <naumov0608@yandex.ru>
2019-10-16 22:21:43 +0300

    corrected task9, added task15

--- task2.c
+++ task2.c
@@ -2,37 +2,77 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct connection
+{
+  int a;
+  int b;
+};
+struct graph
+{
+    int elements_count;
+    struct connection* connections;
+    int connections_count;
+};
+
 int main()
 {
-    int** table;
-    printf("Enter number of graph elements(<100):\n");
+    struct graph g;
+    printf("Enter number of graph elements:\n");
     int n;
     scanf("%d", &n);
-    table = (int**)malloc(sizeof(int*) * n);
-    for(int i = 0; i != n; ++i)
-    {
-        table[i] = (int*)malloc(sizeof(int) * n);
-        memset(table[i], 0, sizeof(int) * n);
-    }
+    g.elements_count = n;
+    
+    g.connections_count = 0;
+    g.connections = (struct connection*)malloc(sizeof(struct connection));
     int finished = 0;
     
     while(!finished)
     {
-        printf("1.Add connection\n2.Finish\n");
+        printf("1.Add connection\n2.Generate graphviz code\n3.Check coherence\n4.Finish\n");
         int in;
         scanf("%d", &in);
         switch(in)
         {
             case 1:
             {
+                g.connections_count++;
+                g.connections = (struct connection*)realloc(g.connections, sizeof(struct connection) * g.connections_count);
                 printf("Type 2 values: ");
                 int a, b;
                 scanf("%d %d", &a, &b);
-                table[a][b] = 1;
-                table[b][a] = 1;
+                g.connections[g.connections_count - 1].a = a;
+                g.connections[g.connections_count - 1].b = b;
+                
                 break;
             }
             case 2:
+            {
+                char* filename = "out.txt";
+//                 printf("Enter filename: ");
+//                 scanf("%s", filename);
+
+                FILE* f = fopen(filename, "w");
+                fprintf(f, "graph g {\n");
+                for(int i = 0; i != g.elements_count; ++i)
+                    fprintf(f, "%d;\n", i);
+                for(int i = 0; i != g.connections_count; ++i)
+                {
+                    fprintf(f, "%d -- %d;\n", g.connections[i].a, g.connections[i].b);
+                }
+                fprintf(f, "}");
+                fclose(f);
+                printf("Code is written to out.txt!\n");
+                break;
+            }
+            case 3:
+            {
+                if(g.connections_count > ((g.elements_count - 1) * (g.elements_count - 2))/2)
+                    printf("This graph is coherent!\n");
+                else
+                    printf("This graph is not coherent!\n");
+                break;
+            }
+            case 4:
             {
                 finished = 1;
                 break;
@@ -45,20 +85,6 @@ int main()
             
         };
     }    
-    printf("\n   ");
-    
-    for(int i = 0; i != n; ++i)
-        printf("%2d ", i);
-    for(int i = 0; i != n; ++i)
-    {
-        printf("\n%2d ", i);
-        for(int j = 0; j != n; ++j)
-        {
-           printf("%2d ", table[i][j]); 
-            
-        }
-        
-    }
-    putchar('\n');
+
     return 0;
 }
